dc7e394721abaa66fa2051e3597ab3b7ef9f7c2e
Carlos Andres Diaz Pinto <98177573+AndresDiazPinto@users.noreply.github.com>
2026-03-11 19:19:58 -0500

    Initial commit
    Se realiza banner, intro, global, img

--- src/config/global.js
+++ src/config/global.js
@@ -1,17 +1,22 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    Name: 'Marketing digital',
+    Description:
+      'Con la llegada de las nuevas tecnologías, el internet ha marcado un gran impacto en todos los aspectos de la vida cotidiana, lo que ha permitido adaptarnos a escenarios desconocidos en una nueva era donde priman los entornos digitales y, en el contexto empresarial, estos aparecen idóneos para el marketing digital, reuniendo una serie elementos y estrategias para lograr los objetivos.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.png'),
       },
       {
         clases: ['banner-principal-decorativo-2'],
         imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.png'),
       },
     ],
   },
@@ -31,28 +36,106 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Conoce el entorno digital',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Componentes del entorno digital',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Plataformas digitales',
+            hash: 't_1_2',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Fundamentos de marketing digital',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Breve historia del marketing digital',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Las 4C del marketing digital',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Herramientas del marketing digital',
+            hash: 't_2_3',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Diferencia entre SEO y SEM. ¿Cómo definir su uso?',
         desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Estrategia digital',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-play-circle',
+            numero: '4.1',
+            titulo: 'Tipos de estrategias',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.2',
+            titulo: 'Plan de marketing digital',
+            hash: 't_4_2',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo: 'Social media',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '5.1',
+            titulo: 'Redes sociales',
+            hash: 't_5_1',
+          },
+          {
+            numero: '5.2',
+            titulo: 'Contenidos para redes sociales',
+            hash: 't_5_2',
+          },
+          {
+            numero: '5.3',
+            titulo: 'Analítica de redes sociales',
+            hash: 't_5_3',
+          },
+          {
+            numero: '5.4',
+            titulo: 'Marco legal de las redes sociales',
+            hash: 't_5_4',
+          },
+          {
+            numero: '5.5',
+            titulo: 'Derechos de autor de contenidos digitales',
+            hash: 't_5_5',
+          },
+          {
+            numero: '5.6',
+            titulo: 'Normatividad colombiana',
+            hash: 't_5_6',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -73,11 +156,11 @@ export default {
         icono: 'fas fa-sort-alpha-down',
         titulo: 'Glosario',
       },
-      {
+      /*{
         nombreRuta: 'complementario',
         icono: 'far fa-folder-open',
         titulo: 'Material complementario',
-      },
+      },*/
       {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
@@ -110,14 +193,174 @@ export default {
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Alcance',
+      significado: 'Determinar hasta qué punto se puede llegar.',
+    },
+    {
+      termino: 'Banners',
+      significado: 'Formato publicitario en Internet.',
+    },
+    {
+      termino: 'Business',
+      significado: 'Traducción al inglés de ‘negocio’.',
+    },
+    {
+      termino: 'Consumer',
+      significado: 'Traducción al inglés de ‘consumidor’.',
+    },
+    {
+      termino: 'Fidelización de clientes',
+      significado:
+        'Proceso para retener consumidores, con el objetivo de que se conviertan en clientes habituales.',
+    },
+    {
+      termino: 'Intrusivo',
+      significado: 'Que se impone sin autorización.',
+    },
+    {
+      termino: 'Jingle',
+      significado:
+        'Canción publicitaria para promocionar un producto o servicio.',
+    },
+    {
+      termino: 'KPI (Key Performance Indicators)',
+      significado:
+        'Indicadores clave de desempeño para el despliegue de una estrategia de marketing.',
+    },
+    {
+      termino: 'Sitio web',
+      significado: 'Conjunto de archivos digitales referentes a un tema.',
+    },
+    {
+      termino: 'Tráfico de usuarios',
+      significado: 'Flujo de información de usuarios en una página web.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Boada, N. (Julio 26 de 2009). <em>Los 8 pasos de un plan de marketing en redes sociales.</em> Cyberclick.',
+      link:
+        'https://www.cyberclick.es/numerical-blog/los-8-pasos-de-un-plan-de-marketing-en-redes-sociales',
+    },
+    {
+      referencia:
+        'Cibrián, I. (2018). <em>Marketing digital: Mide, analiza y mejora.</em> ESIC.',
+    },
+    {
+      referencia:
+        'Cyberclick Academy. (Abril de 2020). <em>¿Qué es el SEM o Search Engine Marketing?</em> (2021).',
+      link: 'https://www.cyberclick.es/sem',
+    },
+    {
+      referencia:
+        'Decisión Andina 351 de 1993. {Comisión del Acuerdo de Cartagena}. <em>Régimen común sobre derecho de autor y derechos conexos.</em> Diciembre 17 de 1993.',
+      link: 'https://www.wipo.int/edocs/lexdocs/laws/es/can/can010es.pdf',
+    },
+    {
+      referencia:
+        'De Castro, I. (Septiembre 30 de 2019). <em>¿Qué son herramientas de marketing y por qué debo contar con ellas? Innovacode - Resolución de problemas.</em>',
+    },
+    {
+      referencia:
+        'De Paul, V. (Diciembre 18 de 2020). <em>20 ideas de redes sociales para mantener el feed de tu marca al día. Sprout Social.</em>',
+      link: 'https://sproutsocial.com/insights/ideas-de-redes-sociales/',
+    },
+    {
+      referencia:
+        'Dotras, A. (2018). <em>Social Media. Herramientas y estrategias empresariales.</em> Ediciones de la U.',
+      link: 'http://www.ebooks7-24.com.bdigital.sena.edu.co/?il=8001',
+    },
+    {
+      referencia:
+        'Giraldo, V. (Febrero 14 de 2019). <em>Plataformas digitales: ¿qué son y qué tipos existen?</em> Rock Content - ES.',
+      link: 'https://rockcontent.com/es/blog/plataformas-digitales/',
+    },
+    {
+      referencia:
+        'Giu, W. (Julio 18 de 2018). <em>Qué es una Estrategia Digital.</em> WalterGiu.',
+      link: 'https://waltergiu.com/blog/que-es-una-estrategia-digital',
+    },
+    {
+      referencia:
+        'Laudon, K., y Laudon, J. (2012). <em>Sistemas de información gerencial.</em> Pearson.',
+      link: 'https://es.calameo.com/read/0045399238b63175162a6',
+    },
+    {
+      referencia:
+        'Ley 23 de 1982. <em>Sobre derechos de autor.</em> Enero 28 de 1982.',
+      link:
+        'https://www.alcaldiabogota.gov.co/sisjur/normas/Norma1.jsp?i=3431&dt=S',
+    },
+    {
+      referencia:
+        'Ley 241 del 2011. <em>Por la cual se regula la responsabilidad por las infracciones al derecho de autor y los derechos conexos en Internet.</em> Abril de 2011.',
+      link:
+        'http://www.informatica-juridica.com/proyecto-de-ley/proyecto-ley-241-derecho-autor-los-derechos-conexos-internet/',
+    },
+    {
+      referencia:
+        'Ley 1834 del 2017. <em>Por medio de la cual se fomenta la economía creativa.</em> Mayo 23 de 2017.',
+      link:
+        'http://www.suin-juriscol.gov.co/viewDocument.asp?ruta=Leyes/30030647',
+    },
+    {
+      referencia:
+        'Ley 1915 del 2018. <em>Por la cual se modifica la ley 23 del 1982 y se establecen otras disposiciones en materia de derecho de autor y derechos conexos.</em> Julio 12 de 2018.',
+      link:
+        'http://es.presidencia.gov.co/normativa/normativa/LEY%201915%20DEL%2012%20DE%20JULIO%20DE%202018.pdf',
+    },
+    {
+      referencia:
+        'MinTIC. (Septiembre 12 de 2015). <em>Redes sociales: Leyes para tener en cuenta.</em> En TIC confío.',
+      link:
+        'https://www.enticconfio.gov.co/redes-sociales-leyes-para-tener-en-cuenta',
+    },
+    {
+      referencia:
+        'Newberry, C. (Julio 17 de 2020). <em>Análisis de redes sociales: Una guía para principiantes. Social Media Marketing & Management Dashboard.</em>',
+      link: 'https://blog.hootsuite.com/es/analisis-de-redes-sociales/',
+    },
+    {
+      referencia: 'OMPI. (2020). <em>¿Qué es la propiedad intelectual?</em>',
+      link: 'https://www.wipo.int/publications/es/details.jsp?id=4528',
+    },
+    {
+      referencia:
+        'Pérez, A. y Acosta, H. (2003). <em>La convergencia mediática: un nuevo escenario para la gestión de información.</em> Revista Cubana de Información en Ciencias de la Salud, 11(1).',
+      link:
+        'http://scielo.sld.cu/scielo.php?script=sci_arttext&pid=S1024-94352003000500003',
+    },
+    {
+      referencia:
+        'Pino, C. (2009). <em>El entorno digital en la nueva era de los medios y la publicidad: la metamorfosis del consumidor.</em> Razón y Palabra, (66).',
+      link: 'https://www.redalyc.org/articulo.oa?id=199520908015',
+    },
+    {
+      referencia: 'SENA, (1989). <em>Hablemos de marketing.</em> 11-13.',
+      link: 'https://hdl.handle.net/11404/876',
+    },
+    {
+      referencia:
+        'Silva, R. (2009). <em>Beneficios del comercio electrónico.</em> Perspectivas, (24), 151-164.',
+      link: 'https://www.redalyc.org/articulo.oa?id=425942160008',
+    },
+    {
+      referencia:
+        'Socialmood. (Marzo 13 de 2020). <em>¿Qué es el SEO y por qué lo necesito?</em> 40deFiebre.',
+      link: 'https://www.40defiebre.com/guia-seo/que-es-seo-por-que-necesito',
+    },
+    {
+      referencia:
+        'Thompson, I. (Octubre de 2006). <em>Definición de Marketing. Portal de marketing - Marketing-free.com.</em>',
+      link:
+        'https://www.marketing-free.com/marketing/definicion-marketing.html',
+    },
+    {
+      referencia:
+        'Xie, Y. M. S. (2019). <em>Marketing digital: Navegando en aguas digitales, sumérgete conmigo.</em> Ediciones de la U.',
+      link:
+        'https://login.bdigital.sena.edu.co/login?url=http://www.ebooks7-24.com/?il=9100',
     },
   ],
   creditos: [
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,10 +8,159 @@
           i.fas.fa-info
       h1 Introducción
     
-    figure.mb-5
-      .video.mb-2
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+    .bg-full-width.bg-fondo-1
+      .px-4.px-lg-5
+        .bloque-texto-g.color-secundario.bg-color-1.p-3.p-sm-4.p-md-5.mb-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/intro/1.png')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0 Apreciado participante, bienvenido a este momento de aprendizaje orientado a las herramientas del entorno digital según el público objetivo y las condiciones de la empresa. Antes de empezar se sugiere revisar la siguiente infografía para identificar el contexto de aprendizaje.
+        
+        div.bg-color-2.tarjeta.p-3.d-inline-block.mb-5
+          h3.mb-0 ¿Cómo surge este curso?
+        
+        p.mb-5 Durante este componente de formación, el marketing digital es el pretexto de aprendizaje y desde donde se direcciona esta experiencia de aprendizaje. Sin embargo, es importante tener presente la información que se puede leer a continuación:
+
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-4.mb-3.mb-lg-0
+            figure
+              img(src="@/assets/curso/intro/2.png", data-aos="zoom-in")
+          .col-lg-8
+            p El marketing digital ha generado un impacto importante en la sociedad, al demostrar que es posible generar nuevas opciones de mercado haciendo uso de las TIC (Tecnologías de la Información y las Comunicaciones) como herramientas de conexión y comercialización.
+            .bg-color-3.p-4
+              p.mb-0 El mundo está conectado en todo momento y lugar a través de las redes sociales. Queriendo o no, éstas se han convertido en un canal imprescindible para desarrollar relaciones interpersonales, promocionar productos y servicios, o impartir educación. En fin, un amplio abanico de posibilidades que se desencadenaron a partir de la industria 4.0. Es aquí donde se observan nuevas oportunidades de desarrollo. Entendiendo que el uso de una red social va más allá de ver memes o estar enterado de la vida de los demás, el marketing digital demuestra que se puede generar desarrollo a través de medios alternativos con amplio alcance a nivel mundial. Pero, ¿qué es el marketing digital? ¿Dónde surge el marketing digital? Para contestar estas y otras preguntas, se sugiere mantener la atención en este componente de formación.
+        
+        div.bg-color-2.tarjeta.p-3.d-inline-block.mb-5
+          h3.mb-0 ¿Cuál es el objeto de aprendizaje?
+        
+        img(src="@/assets/curso/intro/3.png", alt="Imagen decorativa")
+        
+        .row.justify-content-center.align-items-center.z-2.mb-5
+          .col-lg-8(data-aos="fade-up")
+            p.banner-text Como se mencionó anteriormente, el marketing digital es el objeto de aprendizaje que, a través de unas acciones de formación, se espera que desarrolle:
+    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-7.col-xl-4.mb-3.mb-lg-0
+        .tarjeta-avatar-b.align-items-center.mb-3.position-relative(data-aos="fade-up")
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/intro/01.svg' alt='Conocimiento')
+          .tarjeta.tarjeta--azul
+            .p-3
+              h5.mb-0 Conocimiento
+          img.position-absolute.pst(src='@/assets/curso/intro/5.png', style="max-width: 101px" alt='')
+
+        .tarjeta-avatar-b.align-items-center.mb-3.position-relative(data-aos="fade-down")
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/intro/02.svg' alt='')
+          .tarjeta.tarjeta--azul
+            .p-3
+              h5.mb-0 Habilidades
+          img.position-absolute.pst(src='@/assets/curso/intro/6.png', style="max-width: 101px" alt='')
+
+        .tarjeta-avatar-b.align-items-center.mb-3.position-relative(data-aos="fade-up")
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/intro/03.svg' alt='')
+          .tarjeta.tarjeta--azul
+            .p-3
+              h5.mb-0 Actitudes
+          img.position-absolute.pst(src='@/assets/curso/intro/7.png', style="max-width: 101px" alt='')
+      .col-md-8.col-lg-5.col-xl-3.mb-3.mb-xl-0
+        figure
+          img(src="@/assets/curso/intro/4.png", data-aos="zoom-in")
+      .col-xl-3
+        .bg-color-3.p-4
+          h5 Algunos temas que se verán son:
+          ul.lista-ul--color.ms-3
+            li.mb-3.d-flex
+              i.fas.fa-angle-right.bg-color-4
+              | Entrono digital
+            li.mb-3.d-flex
+              i.fas.fa-angle-right.bg-color-4
+              | Fundamentos del marketing digital
+            li.mb-3.d-flex
+              i.fas.fa-angle-right.bg-color-4
+              | Comercio electrónico
+            li.mb-3.d-flex
+              i.fas.fa-angle-right.bg-color-4
+              | Estrategia digital
+            li.mb-3.d-flex
+              i.fas.fa-angle-right.bg-color-4
+              | Social media
+
+    div.bg-color-2.tarjeta.p-3.d-inline-block.mb-5
+      h3.mb-0 ¿Cómo está estructurado el componente de formación?
+
+    .bloque-texto-g.color-secundario.bg-color-1.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/intro/8.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 El curso virtual está estructurado a partir de componentes de formación que contienen la información necesaria para que se puedan desarrollar conocimientos, habilidades y actitudes en el marco de la competencia estructuración de la estrategia de marketing digital acorde con los objetivos de campaña.
+
+
+    .row.align-items-center.bg-color-5.tarjeta.p-4.mb-5
+      .col-lg-auto.mb-3.mb-lg-0.order-2
+        img.d-lg-flex.d-none(src="@/assets/curso/intro/9.png").mx-auto
+      .col-lg.order-1.mb-3.mb-lg-0
+        p.mb-0 En cada componente, es posible encontrar diferentes recursos de aprendizaje y unas actividades de aprendizaje. Es necesario buscar la guía de actividades de aprendizaje para desarrollarla.
+
+    div.bg-color-2.tarjeta.p-3.d-inline-block.mb-5
+      h3.mb-0 ¿Quiénes están en el curso?
+
+    p.mb-5 Los cursos virtuales son entornos de aprendizaje donde convergen:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-8.col-lg-5.col-xl-4.mb-3.mb-xl-0
+        figure
+          img(src="@/assets/curso/intro/10.png", data-aos="zoom-in")
+      .col-lg-7.col-xl-3.mb-3.mb-lg-0
+        .tarjeta-avatar-b.align-items-center.mb-5.position-relative(data-aos="fade-up")
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/intro/01.svg' alt='')
+          .tarjeta.tarjeta--azul
+            .p-4
+              h5.mb-0 Contenidos
+          img.position-absolute.pst(src='@/assets/curso/intro/11.png', style="max-width: 101px" alt='')
+
+        .tarjeta-avatar-b.align-items-center.mb-5.position-relative(data-aos="fade-down")
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/intro/02.svg' alt='')
+          .tarjeta.tarjeta--azul
+            .p-4
+              h5.mb-0 Recursos
+          img.position-absolute.pst(src='@/assets/curso/intro/12.png', style="max-width: 101px" alt='')
+
+        .tarjeta-avatar-b.align-items-center.mb-5.position-relative(data-aos="fade-up")
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/intro/03.svg' alt='')
+          .tarjeta.tarjeta--azul
+            .p-4
+              h5.mb-0 Personas
+          img.position-absolute.pst(src='@/assets/curso/intro/13.png', style="max-width: 101px" alt='')
+      .col-xl-5
+        .bg-color-3.p-4.mb-3
+          p.mb-0 Que de una forma intencionada constituyen una comunidad de aprendizaje y práctica. Es decir, no se está solo, se tiene un conjunto de personas de diferentes lugares que se unen a las reflexiones e inquietudes. Por eso, es preciso recordar la importancia de participar en los foros y utilizar otras herramientas del curso para comunicarse.
+        p Los cursos virtuales son entornos de aprendizaje donde convergen:
+        ul.lista-ul--color.ms-3
+          li.mb-3.d-flex
+            i.fas.fa-angle-right.bg-color-4
+            | Organizar un horario de estudio individual y colaborativo.
+          li.mb-3.d-flex
+            i.fas.fa-angle-right.bg-color-4
+            | Revisar las notificaciones del curso constantemente.
+          li.mb-3.d-flex
+            i.fas.fa-angle-right.bg-color-4
+            | Participar en los foros y actividades sugeridas por el instructor.
+          li.mb-3.d-flex
+            i.fas.fa-angle-right.bg-color-4
+            | Entregar las evidencias esperadas durante este componente de formación.
+
+    .cajon.color-acento-botones.bg-color-6.p-4.mb-4
+      p.mb-0 Si en el desarrollo del curso surge alguna duda o requiere orientación adicional, recuerde que cuenta con el acompañamiento del equipo formador. No dude en comunicarse a través de los canales establecidos en la plataforma; estaremos atentos para apoyarle en su proceso de aprendizaje.
+
+
+    
 
 </template>
 
